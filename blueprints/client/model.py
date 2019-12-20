da944fe09c4911fb8015b67eabf5e4fe4f357dd0
Garry Ariel <garry@alterra.id>
2019-12-20 20:51:57 +0700

    Auth Finished

--- blueprints/client/model.py
+++ blueprints/client/model.py
@@ -7,11 +7,13 @@ class Client(db.Model):
     __tablename__ = 'client'
     client_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     name = db.Column(db.String(255), nullable = False)
+    username = db.Column(db.String(255), nullable = False, unique = True)
     password = db.Column(db.String(255), nullable = False)
 
     client_fields = {
         'client_id': fields.Integer,
         'name': fields.String,
+        'username': fields.String,
         'password': fields.String
     }
 
@@ -19,9 +21,10 @@ class Client(db.Model):
     #     'client_key': fields.String,
     # }
 
-    def __init__(self, client_id, name, password):
+    def __init__(self, client_id, name, username, password):
         self.client_id = client_id
         self.name = name
+        self.username = username
         self.password = password
 
     def __repr__(self):
--- blueprints/client/resources.py
+++ blueprints/client/resources.py
@@ -21,54 +21,54 @@ bp_client = Blueprint('client', __name__)
 api = Api(bp_client)
 
 class ClientResource(Resource):
-    # Get By ID
-    @jwt_required
-    @internal_required
-    def get(self, id=None):
-        qry = Client.query.get(id)
-        if qry:
-            return marshal(qry, Client.client_fields), 200
-        return {'status': 'NOT FOUND'}, 404
+#     # Get By ID
+#     @jwt_required
+#     @internal_required
+#     def get(self, id=None):
+#         qry = Client.query.get(id)
+#         if qry:
+#             return marshal(qry, Client.client_fields), 200
+#         return {'status': 'NOT FOUND'}, 404
 
-    # Put
-    @jwt_required
-    @internal_required
-    def put(self, id=None):
-        client = Client.query.get(id)
-        client = marshal(client, Client.client_fields)
+#     # Put
+#     @jwt_required
+#     @internal_required
+#     def put(self, id=None):
+#         client = Client.query.get(id)
+#         client = marshal(client, Client.client_fields)
 
-        if client['client_id'] != 0:
-            parser = reqparse.RequestParser()
-            parser.add_argument('client_id', type=int, location='json', required=True)
-            parser.add_argument('client_key', location='json', required=True)
-            parser.add_argument('client_secret', location='json', required=True)
-            parser.add_argument('status', location='json',type=inputs.boolean, required=True)
-            args = parser.parse_args()
+#         if client['client_id'] != 0:
+#             parser = reqparse.RequestParser()
+#             parser.add_argument('client_id', type=int, location='json', required=True)
+#             parser.add_argument('client_key', location='json', required=True)
+#             parser.add_argument('client_secret', location='json', required=True)
+#             parser.add_argument('status', location='json',type=inputs.boolean, required=True)
+#             args = parser.parse_args()
 
-            # Updated the object
-            client['client_id'] = args['client_id']
-            client['client_key'] = args['client_key']
-            client['client_secret'] = args['client_secret']
-            client['status'] = args['status']
-            client['updated_at'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-            db.session.commit()
-            app.logger.debug('DEBUG : %s', client)
+#             # Updated the object
+#             client['client_id'] = args['client_id']
+#             client['client_key'] = args['client_key']
+#             client['client_secret'] = args['client_secret']
+#             client['status'] = args['status']
+#             client['updated_at'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+#             db.session.commit()
+#             app.logger.debug('DEBUG : %s', client)
 
-            return client, 200, {'Content-Type':'application/json'}
-        return {'status' : 'NOT FOUND'}, 404, {'Content-Type':'application/json'}
+#             return client, 200, {'Content-Type':'application/json'}
+#         return {'status' : 'NOT FOUND'}, 404, {'Content-Type':'application/json'}
 
-    # Delete
-    @jwt_required
-    @internal_required
-    def delete(self, id=None):
-        client = Client.query.get(id)
+#     # Delete
+#     @jwt_required
+#     @internal_required
+#     def delete(self, id=None):
+#         client = Client.query.get(id)
 
-        if client is not None:
-            # Hard Delete
-            db.session.delete(client)
-            db.session.commit()
-            return {'status': 'DELETED'}, 200, {'Content-Type':'application/json'}
-        return {'status' : 'NOT FOUND'}, 404, {'Content-Type':'application/json'}
+#         if client is not None:
+#             # Hard Delete
+#             db.session.delete(client)
+#             db.session.commit()
+#             return {'status': 'DELETED'}, 200, {'Content-Type':'application/json'}
+#         return {'status' : 'NOT FOUND'}, 404, {'Content-Type':'application/json'}
 
 class ClientList(Resource):
     # Get All
@@ -103,36 +103,36 @@ class ClientList(Resource):
             filter_result.append(marshal(query, Client.client_fields))
         return filter_result
     
-    # Post
-    @jwt_required
-    @internal_required
-    def post(self):
-        # Setup the policy
-        policy = PasswordPolicy.from_names(
-            length = 8
-        )
+#     # Post
+#     @jwt_required
+#     @internal_required
+#     def post(self):
+#         # Setup the policy
+#         policy = PasswordPolicy.from_names(
+#             length = 8
+#         )
 
-        parser = reqparse.RequestParser()
-        parser.add_argument('client_id', type=int, location='json', required=True)
-        parser.add_argument('client_key', location='json', required=True)
-        parser.add_argument('client_secret', location='json', required=True)
-        parser.add_argument('status', location='json',type=inputs.boolean, required=True)
-        args = parser.parse_args()
+#         parser = reqparse.RequestParser()
+#         parser.add_argument('client_id', type=int, location='json', required=True)
+#         parser.add_argument('client_key', location='json', required=True)
+#         parser.add_argument('client_secret', location='json', required=True)
+#         parser.add_argument('status', location='json',type=inputs.boolean, required=True)
+#         args = parser.parse_args()
 
-        # Validating the password policy
-        validation = policy.test(args['client_secret'])
+#         # Validating the password policy
+#         validation = policy.test(args['client_secret'])
 
-        if validation == []:
-            password_digest = hashlib.md5(args['client_secret'].encode()).hexdigest()
-            # Creating object
-            client = Client(args['client_id'], args['client_key'], password_digest, args['status'])
-            db.session.add(client)
-            db.session.commit()
+#         if validation == []:
+#             password_digest = hashlib.md5(args['client_secret'].encode()).hexdigest()
+#             # Creating object
+#             client = Client(args['client_id'], args['client_key'], password_digest, args['status'])
+#             db.session.add(client)
+#             db.session.commit()
 
-            app.logger.debug('DEBUG : %s', client)
+#             app.logger.debug('DEBUG : %s', client)
 
-            return marshal(client, Client.client_fields), 200, {'Content-Type':'application/json'}
-        return {'status': 'Failed'}
+#             return marshal(client, Client.client_fields), 200, {'Content-Type':'application/json'}
+#         return {'status': 'Failed'}
 
 api.add_resource(ClientList, '', '/list')
 api.add_resource(ClientResource, '/<id>')
